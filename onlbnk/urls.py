2c466968e941ba8d6928731f5fda0ab7aa85f6c7
DFB Nigeria <itdesk@dfbnigeria.org.ng>
2021-01-31 17:03:47 +0100

    Change Url structure

--- onlbnk/urls.py
+++ onlbnk/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', include('pages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
